402279170e8b9edfbeab153091870964d1741b5c
torekhan <torekhan93@mail.ru>
2017-12-23 14:58:20 +0000

    Express REST and Router

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express'),
 http = require('http'),
-morgan = require('morgan');
+morgan = require('morgan'),
+bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
@@ -16,7 +22,6 @@ app.use((req, res, next) => {
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
-
 
 const server = http.createServer(app);
 
