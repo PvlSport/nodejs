dc6d8ef232f1f6eee94746315d9a838c44a27cad
torekhan <torekhan93@mail.ru>
2017-12-24 22:37:06 +0000

    Mongoose ODM

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
-const dboper = require('./operations');
+const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, {
+    useMongoClient: true
+});
 
-MongoClient.connect(url).then((client) => {    
-
+connect.then((db) => {
     console.log('Connected correctly to server');
-    const db = client.db('conFusion');
-    
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test"}, "dishes")
-    .then((result) => {
-        console.log("Insert Document:\n", result.ops);
 
-        return dboper.findDocuments(db, "dishes");
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
     })
-    .then((docs) => {
-        console.log("Found Documents:\n", docs);
+    .then((dish) => {
+        console.log(dish);
 
-        return dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, "dishes");
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((result) => {
-        console.log("Updated Document:\n", result.result);
+    .then((dish) => {
+        console.log(dish);
 
-        return dboper.findDocuments(db, "dishes");
-    })
-    .then((docs) => {
-        console.log("Found Updated Documents:\n", docs);
-                        
-        return db.dropCollection("dishes");
-    })
-    .then((result) => {
-        console.log("Dropped Collection: ", result);
+        dish.comments.push({
+            rating: 5,
+            comment: 'Im getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
 
-        return client.close();
+        return dish.save();
     })
-    .catch((err) => console.log(err));
+    .then((dish) => {
+        console.log(dish);
 
-})
-.catch((err) => console.log(err));
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+});
